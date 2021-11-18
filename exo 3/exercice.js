39a3aad113f4353024ad9c3e937eca15c653d7fb
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 10:02:58 +0100

    cible et ajoute un é

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,8 @@
+let space = document.getElementsByClassName('name-tag');
 
+for(let i = 0 ; i < space.length ; i++){
+    let newP = document.createElement('p');
+    newP.innerHTML = 'HELLO WORLD';
 
-
-
-
-
-
-
-
-
+    space[i].appendChild(newP);
+}
